3bfaf822ebd194dc93497bd68b1ea06348e3f772
matu184 <matu184@live.de>
2023-12-18 12:05:29 +0100

    Projektdateien hinzufügen.

---
+++ Programmierer/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Programmierer.Models;
+using System.Diagnostics;
+
+namespace Programmierer.Controllers
+{
+
+
+    public class HomeController : Controller
+    {
+        private List<string> Projekte = new List<string> { "Webanwendung", "WPF Anwendungen ", "JavaScript lernen", "C# Vertiefung", "Pause" };
+        private List<string> Aufgaben = new List<string> { "Programmieren", "Testen", "Dokumentieren", "Pause" };
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewData["Projekte"] = new SelectList(Projekte);
+            ViewData["Aufgaben"] = new SelectList(Aufgaben);
+
+            return View(new Programmer());
+        }
+        [HttpPost]
+        public IActionResult Index(Programmer neu)
+        {
+
+            return View(neu);
+        }
+
+
+    }
+}
---
+++ Programmierer/Models/Programmer.cs
@@ -0,0 +1,28 @@
+﻿namespace Programmierer.Models
+{
+    public enum Geschlecht
+    {
+        Männlich,
+        Weiblich,
+        
+    }
+    public class Programmer
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public string Email { get; set; }
+        public Geschlecht Geschlecht { get; set; }
+        public List<string> Aufgaben { get; set; }
+        public List<string> Projekte { get; set;}
+
+        public Programmer()
+        {
+            Aufgaben = new List<string>();
+            Projekte = new List<string>();
+        }
+
+        
+        
+    }
+
+}
